42e742ca516fd5ff21055a6cc0a3c32cd9c88bb7
phani.pemmaraju <phani.pemmaraju@digital.hmrc.gov.uk>
2020-03-14 23:50:20 +0000

    GCP stackdriver logging added.

--- src/main/java/uk/co/legacy/it/api/resources/HelloResource.java
+++ src/main/java/uk/co/legacy/it/api/resources/HelloResource.java
@@ -1,6 +1,7 @@
 package uk.co.legacy.it.api.resources;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value="/hello")
+@Slf4j
 public class HelloResource {
 
     @GetMapping
     public String defaultHello() {
+        log.info("defaultHello method called ");
         return "Hello to everyone !!!";
     }
 
     @GetMapping(value = "/{name}")
     public String sayHello(@PathVariable String name) {
+        log.info("sayHello method called for parameter {}  ", name);
         return "Hello !!! " + name;
     }
 
