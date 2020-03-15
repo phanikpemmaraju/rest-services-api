8a335ec774f56c2ebcfe935229acf784318bb0cb
phani.pemmaraju <phani.pemmaraju@digital.hmrc.gov.uk>
2020-03-15 20:25:40 +0000

    Added license information to open api documentation.

--- src/main/java/uk/co/legacy/it/config/OpenApiConfig.java
+++ src/main/java/uk/co/legacy/it/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package uk.co.legacy.it.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +14,11 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final License license = new License().name("Apache License Version 2.0").url("https://www.apache.org/licenses/LICENSE-2.0");
         return new OpenAPI().info(new Info()
                                     .title("Legacy IT Rest Services API")
                                     .version("1.0.0")
-                                    .description("Rest APIs exposed"));
+                                    .description("Rest APIs exposed")
+                                    .license(license));
     }
 }
